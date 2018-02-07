5ed9524ee49fe369a2d9de8d950cf503e938ef42
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2018-02-07 19:50:17 +0100

    Move requirements to setup

--- setup.py
+++ setup.py
@@ -8,11 +8,16 @@ AUTHOR = 'Adrian Seyboldt'
 AUTHOR_EMAIL = 'adrian.seyboldt@gmail.com'
 VERSION = '0.1'
 
-PROJECT_ROOT = dirname(realpath(__file__))
-REQUIREMENTS_FILE = join(PROJECT_ROOT, 'requirements.txt')
+requirements = [
+    'Bottleneck>=1.1',
+    'pymc3>=3.2',
+    'scipy>=0.19.0',
+    'xarray>=0.9',
+    'seaborn',
+    'cvxpy<1.0',
+    'empyrical',
+]
 
-with open(REQUIREMENTS_FILE) as reqfile:
-    install_reqs = reqfile.read().splitlines()
 
 if __name__ == "__main__":
     setup(
@@ -20,6 +25,6 @@ if __name__ == "__main__":
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
         packages=find_packages(),
-        install_requires=install_reqs,
+        install_requires=requirements,
         test_requires=['pytest']
     )
