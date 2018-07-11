21ce56dd9928110442ae50f98b6375d245168ac6
George Ho <georgeho1618@gmail.com>
2018-07-11 14:32:03 -0400

    BLD: make base module

---
+++ bayesalpha/base.py
@@ -0,0 +1,64 @@
+import json
+import hashlib
+import xarray as xr
+
+
+class BaseFitResult:
+    def __init__(self, trace):
+        self._trace = trace
+
+    def save(self, filename, group=None, **args):
+        """Save the results to a netcdf file."""
+        self._trace.to_netcdf(filename, group=group, **args)
+
+    @classmethod
+    def _load(cls, filename, group=None):
+        trace = xr.open_dataset(filename, group=group)
+        return cls(trace=trace)
+
+    @property
+    def trace(self):
+        return self._trace
+
+    @property
+    def params(self):
+        return json.loads(self._trace.attrs['params'])
+
+    @property
+    def timestamp(self):
+        return self._trace.attrs['timestamp']
+
+    @property
+    def model_version(self):
+        return self._trace.attrs['model-version']
+
+    @property
+    def params_hash(self):
+        params = json.dumps(self.params, sort_keys=True)
+        hasher = hashlib.sha256(params.encode())
+        return hasher.hexdigest()[:16]
+
+    @property
+    def ok(self):
+        return len(self.warnings) == 0
+
+    @property
+    def warnings(self):
+        return json.loads(self._trace.attrs['warnings'])
+
+    @property
+    def seed(self):
+        return self._trace.attrs['seed']
+
+    @property
+    def id(self):
+        hasher = hashlib.sha256()
+        hasher.update(self.params_hash.encode())
+        hasher.update(self.model_version.encode())
+        hasher.update(str(self.seed).encode())
+        return hasher.hexdigest()[:16]
+
+    def raise_ok(self):
+        if not self.ok:
+            warnings = self.warnings
+            raise RuntimeError('Problems during sampling: %s' % warnings)
