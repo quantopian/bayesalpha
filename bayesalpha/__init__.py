595a92aab2d9be54b8d14cc1cdd9ff26de2b4ce6
George Ho <georgeho1618@gmail.com>
2018-07-12 09:17:56 -0400

    MAINT: fix init

--- bayesalpha/__init__.py
+++ bayesalpha/__init__.py
@@ -1,4 +1,5 @@
-from .model import fit_single, fit_population, load
+from .model import fit_single, fit_population
+from .author_model import fit_authors
 
 from ._version import get_versions
 __version__ = get_versions()['version']
