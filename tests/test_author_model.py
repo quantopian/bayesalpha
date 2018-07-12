eaa8f87a78b2c0f033dcbbe000175e3f2baace4c
George Ho <georgeho1618@gmail.com>
2018-07-12 14:31:17 -0400

    TST: added pytest fixture

--- tests/test_author_model.py
+++ tests/test_author_model.py
@@ -1,8 +1,15 @@
-import numpy as np
+import os
 import pandas as pd
-import pymc3 as pm
 import bayesalpha
 import pytest
+
+
+@pytest.fixture
+def data():
+    __location__ = os.path.realpath(os.path.join(os.getcwd(),
+                                    os.path.dirname(__file__)))
+    return pd.read_csv(__location__ + 'test_data/author_model_test_data.csv',
+                       index_col=0)
 
 
 def test_fit_author_model(data):
